a022fab55f5582479a85dbbd29203b76943ad5e9
Samuelangel <samuel.angel7650@alumnos.udg.mx>
2024-04-16 16:46:37 -0600

    (feat)->Diseno pagina inicio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,13 +6,13 @@
 @vite(['resources/sass/sideBar.scss', 'resources/sass/home.scss', 'resources/js/app.js'])
 @endsection
 
-@section('titleView','Inicio')
+@section('titleView','Estadistica')
 
 
 @section('breadCrumb')
 <nav aria-label="breadcrumb">
     <ol class="breadcrumb">
-        <li class="breadcrumb-item active" aria-current="page">Inicio</li>
+        <li class="breadcrumb-item active" aria-current="page">Revisa la ultima información</li>
     </ol>
 </nav>
    
@@ -24,7 +24,47 @@
     <div class="row justify-content-center">
         <div class="col-12 text-center border-bottom mt-0">
             <h4 class="fst-italic"> Bienvenido de nuevo</h4>
+            <p>Revisa la ultima informacion</p>
         </div>
+    </div>
+</div>
+<br><br><br>
+<div class="container">
+    <div class="mx-auto" style="width: 1000px;">
+        <p>
+            <div class="container-xl">
+                <ul class="list-group list-group-horizontal">
+                    <li class="list-group-item">
+                        <font size=5 color="4f514e">Citas pendientes</font> 
+                        <br>
+                        <font color="56a72e" size=4>5 Citas</font>
+                        <br><br>
+                        20 Marzo
+                    </li>
+                    <li class="list-group-item">
+                        <font size=5 color="4f514e">Citas pendientes</font> 
+                        <br>
+                        <font color="56a72e" size=4>5 Citas</font>
+                        <br><br>
+                        20 Marzo
+                    </li>
+                    <li class="list-group-item">
+                        <font size=5 color="4f514e">Citas pendientes</font> 
+                        <br>
+                        <font color="56a72e" size=4>5 Citas</font>
+                        <br><br>
+                        20 Marzo
+                    </li>
+                    <li class="list-group-item">
+                        <font size=5 color="4f514e">Citas pendientes</font> 
+                        <br>
+                        <font color="56a72e" size=4>5 Citas</font>
+                        <br><br>
+                        20 Marzo
+                    </li>
+                </ul>
+            </div>
+        </p>
     </div>
 </div>
 
