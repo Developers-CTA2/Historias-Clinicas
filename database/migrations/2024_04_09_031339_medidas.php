66fe0e12a8b97ce89c9b08f12b58b7a14ee24b50
Juan Pedro Domínguez Padilla <juan.dominguez@cualtos.udg.mx>
2024-07-03 16:32:20 -0600

    Modifications in migrations

--- database/migrations/2024_04_09_031339_medidas.php
+++ database/migrations/2024_04_09_031339_medidas.php
@@ -12,12 +12,12 @@ return new class extends Migration
     public function up()
     {
         Schema::create('medidas', function (Blueprint $table) {
+            $table->id('id_medida');
             $table->double('peso_actual');
             $table->double('peso_habitual');
             $table->double('estatura');
-            $table->double('circunf_cintura');
-            $table->double('circunf_cadera');
-            $table->foreignId('id_nutricional')->constrained('nutricional', 'id_nutricional');
+            $table->double('circunferencia_cintura');
+            $table->double('circunferencia_cadera');
         });
     }
 
@@ -26,10 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-
-        Schema::table('medidas', function (Blueprint $table) {
-            $table->dropForeign(['id_nutricional']);
-        });
 
         Schema::dropIfExists('medidas');
     }
