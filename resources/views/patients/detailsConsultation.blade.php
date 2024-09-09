8832c6dc2827307e451b1bc0c244bcf8e4a4399d
J. Pedro <juan.dominguez2763@alumnos.udg.mx>
2024-09-09 12:03:18 -0600

    Fixed user interface of the citas

--- resources/views/patients/detailsConsultation.blade.php
+++ resources/views/patients/detailsConsultation.blade.php
@@ -19,7 +19,7 @@
 
         <div class="row mt-3">
             <div class="container">
-                <div class="row">
+                <div class="row d">
                     <div class="col-12 col-xl-9">
 
                         {{-- Card de datos de la consulta --}}
