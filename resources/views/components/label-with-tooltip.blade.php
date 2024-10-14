eeaeb7727f6b29d032c4e0d139e0ff4a66302255
Juan Pedro Dominguez <juan.dominguez2763@alumnos.udg.mx>
2024-10-14 14:09:44 -0600

    Update component

--- resources/views/components/label-with-tooltip.blade.php
+++ resources/views/components/label-with-tooltip.blade.php
@@ -1,6 +1,6 @@
 <div class="d-flex justity-content-between">
     <label for="{{$labelFor}}" class="flex-grow-1"> @if($required)<span class="required-point me-1">*</span>@endif{{$titleLabel}}</label>
-    @if($haveTooltip)
+    @if(isset($haveTooltip) && $haveTooltip)
         <x-tooltip message="{{$message}}" />
     @endif
 </div>
