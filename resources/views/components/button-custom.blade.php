5495cc8fc3bf25f695d201356c6efe58cc77e656
Juan Pedro Dominguez <juan.dominguez2763@alumnos.udg.mx>
2024-10-14 09:29:46 -0600

    Buttom custom test

--- resources/views/components/button-custom.blade.php
+++ resources/views/components/button-custom.blade.php
@@ -1,4 +1,4 @@
-<button {{ $attributes }} @class(['fst-normal' ,'tooltip-container','d-flex','gap-1',  $class, $padding]) id="{{$id}}" {{$disabled ? 'disabled' : ''}} >
+<button {{ $attributes }} @class(['fst-normal' ,'tooltip-container','d-flex','gap-1',  $class, $paddingClass]) id="{{$id}}" {{$disabled ? 'disabled' : ''}} >
     <div @class(['me-1'=>  !$onlyIcon]) >
         {!! $icon !!}
     </div>
