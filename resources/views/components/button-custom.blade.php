efbc68a31fb8688b7691110b3f48a630aaf1da8e
Juan Pedro Dominguez <juan.dominguez2763@alumnos.udg.mx>
2024-10-14 11:28:00 -0600

    Update component and construct

--- resources/views/components/button-custom.blade.php
+++ resources/views/components/button-custom.blade.php
@@ -1,5 +1,5 @@
 <button {{ $attributes->merge(['class' => 'fst-normal tooltip-container d-flex gap-1 ' . ($class ?? 'btn-primary') . ' ' . ($paddingClass ?? 'px-3 py-2'), 'id' => $id, 'type' => ($typeButton ?? 'button') , 'disabled' => ($disabled ?? '')]) }}  >
-    <div @class(['me-1'=>  !$onlyIcon]) >
+    <div @class(['me-1'=>  !($onlyIcon ?? false)]) >
         {!! $icon !!}
     </div>
     {{$text}}
