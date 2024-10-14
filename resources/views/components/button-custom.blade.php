2aa1bbb233facb16c96920b2132ce360d493c795
Juan Pedro Dominguez <juan.dominguez2763@alumnos.udg.mx>
2024-10-14 09:55:54 -0600

    Buttom custom test

--- resources/views/components/button-custom.blade.php
+++ resources/views/components/button-custom.blade.php
@@ -1,9 +1,8 @@
-<button {{ $attributes }} @class(['fst-normal' ,'tooltip-container','d-flex','gap-1',  $class, $paddingClass ?? 'px-3 py-2']) id="{{$id}}" {{$disabled ? 'disabled' : ''}} >
+<button {{ $attributes->merge(['class' => 'fst-normal tooltip-container d-flex gap-1 ' . $class . ' ' .$paddingClass ?? ' px-3 py-2', 'id' => $id, 'type' => $type, 'disabled' => $disabled]) }}  >
     <div @class(['me-1'=>  !$onlyIcon]) >
         {!! $icon !!}
     </div>
     {{$text}}
-    {{ $padding}}
     <span class="tooltip-text">{{$tooltipText}}</span>
 </button>
 
