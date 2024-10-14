ae6ad4c5e602f3eabbd805e147419b24644ef1e4
Juan Pedro Dominguez <juan.dominguez2763@alumnos.udg.mx>
2024-10-14 11:14:38 -0600

    Update component and construct

--- app/View/Components/buttonCustom.php
+++ app/View/Components/buttonCustom.php
@@ -12,42 +12,39 @@ class buttonCustom extends Component
      * Create a new component instance.
      */
 
-    public $type;
-    public $class;
-    public $text;
-    public $id;
-    public $icon;
-    public $tooltipText;
-    public $disabled;
-    public $paddingClass;
-    public $onlyIcon;
+     public $type;
+     public $class;
+     public $text;
+     public $id;
+     public $icon;
+     public $tooltipText;
+     public $disabled;
+     public $paddingClass; // Define la propiedad correctamente
+     public $onlyIcon;
+ 
+     public function __construct(
+         string $type = 'button',
+         string $class = 'btn-primary',
+         string $text = '',
+         string $id = '',
+         string $icon = '',
+         string $tooltipText = '',
+         bool $disabled = false,
+         string $paddingClass = 'px-3 py-2', // Aquí mantenemos el nombre consistente
+         bool $onlyIcon = false
+     ) {
 
-
-    public function __construct(
-        string $type = 'button',
-        string $class = 'btn-primary',
-        string $text = '',
-        string $id = '',
-        string $icon = '',
-        string $tooltipText = '',
-        bool $disabled = false,
-        string $paddingClass = '',
-        bool $onlyIcon = false
-    )
-    {
-        $this->type = $type;
-        $this->class = $class;
-        $this->text = $text;
-        $this->id = $id;
-        $this->icon = $icon;
-        $this->tooltipText = $tooltipText;
-        $this->disabled = $disabled;
-        $this->paddingClass = $paddingClass ?? 'px-3 py-2';
-        $this->onlyIcon = $onlyIcon;
-
-        dd($this->paddingClass);
-        
-    }
+        dd($paddingClass);
+         $this->type = $type;
+         $this->class = $class;
+         $this->text = $text;
+         $this->id = $id;
+         $this->icon = $icon;
+         $this->tooltipText = $tooltipText;
+         $this->disabled = $disabled;
+         $this->onlyIcon = $onlyIcon;
+         $this->paddingClass = $paddingClass; // Asegúrate de usar el mismo nombre
+     }
 
     /**
      * Get the view / contents that represent the component.
--- resources/views/components/button-custom.blade.php
+++ resources/views/components/button-custom.blade.php
@@ -1,4 +1,4 @@
-<button {{ $attributes->merge(['class' => 'fst-normal tooltip-container d-flex gap-1 ' . $class . ' ' . $paddingClass, 'id' => $id, 'type' => $type, 'disabled' => $disabled]) }}  >
+<button {{ $attributes->merge(['class' => 'fst-normal tooltip-container d-flex gap-1 ' . ($class ?? '') . ' ' . ($paddingClass ?? ''), 'id' => $id, 'type' => $type, 'disabled' => $disabled ? 'disabled' : null]) }}  >
     <div @class(['me-1'=>  !$onlyIcon]) >
         {!! $icon !!}
     </div>
